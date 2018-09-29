16ea89839956a406c82f5c574711c649a56a80c2
pabloabadsanz <pabloabadsanz@gmail.com>
2018-09-29 15:58:17 +0200

    Different environments

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.ENV.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.ENV.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.ENV.MONGO_URI,
+  cookieKey: process.ENV.COOKIE_KEY
+}
